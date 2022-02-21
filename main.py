95de4fa626c80c6fc5e37f3985f2f70cdb5b503f
pedroski121 <obipedro121@gmail.com>
2022-02-22 00:10:02 +0100

    Articles, login and registeration pages design

--- main.py
+++ main.py
@@ -8,23 +8,35 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
+@app.route('/article-content')
+def article_content():
+    return render_template('article-content.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 @app.route('/our-story')
 def story():
     return 'OUR STORY'
 
 
-@app.route('/login')
-def login():
-    return 'Login'
-
-@app.route('/articles')
-def articles():
-    return 'Articles'
-
 @app.route('/donate')
 def donate():
     return 'Donate'
-
 
 
 print(__name__)
